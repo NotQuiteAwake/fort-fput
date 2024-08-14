898f490d04804609cbc365cca4f9abbe81842a7f
Jimmy C <11169901+NotQuiteAwake@users.noreply.github.com>
2024-08-14 01:22:17 +0100

    improve io functionalities

--- src/fput.f90
+++ src/fput.f90
@@ -4,12 +4,13 @@ program fput
     use integrator, only: symp_integrator
     use modes
     use forces
+    use io
 
     implicit none
 
     logical :: verbose = .false.
     integer(i4) :: size, steps, order, mode, i
-    integer(i4), parameter :: OUTLEN = 20
+    integer(i4), parameter :: OUTLEN = 20, NUM_MODES = 20
     real(dp) :: step_size, lat_param, mass, amp, cycle_threshold
     type(grid) :: string 
     type(force_param) :: f_param
@@ -22,28 +23,50 @@ program fput
     read(*, *) cycle_threshold
 
     if (verbose) then
-        write(*, '(A20, I8)') "Size: ", size
-        write(*, '(A20, F8.3)') "Lattice parameter: ", lat_param
-        write(*, '(A20, F8.3)') "Mass: ", mass
-        write(*, '(A20, F8.3)') "Amplitude: ", amp
-        write(*, '(A20, I8)') "Initial steps: ", steps
-        write(*, '(A20, F8.3)') "Step size: ", step_size
-        write(*, '(A20, I8)') "Integrator order: ", order
-        write(*, '(A20, F8.3, F8.3)') "Force parameters: ", f_param%k, f_param%alpha
-        write(*, '(A20, F8.3)') "Cycle threshold: ", cycle_threshold
+        call write_params()
     end if
 
     string = create_grid(size, lat_param, mass)
     string%y = gen_nth_normal(string, mode, amp)
 
     do i = 1, steps
-        write(*, '(*(F7.4,2X))') decompose(string, 5)
-        string = symp_integrator(string, nonlinear_f, f_param, step_size, order)     
+        call log()
+        call evolve()
     end do
 
     do while (abs(1 - abs(get_comp(string, mode))) > cycle_threshold)
-        write(*, '(*(F7.4,2X))') decompose(string, 5)
-        string = symp_integrator(string, nonlinear_f, f_param, step_size, order)     
+        call log()
+        call evolve()
     end do
+
+contains
+
+    subroutine write_params()
+        call log_param("size", size)
+        call log_param("lattice param", lat_param)
+        call log_param("mass", mass)
+
+        call log_param("steps", steps)
+        call log_param("step size", step_size)
+        call log_param("integrator order", order)
+
+        call log_param("initial normal mode", mode)
+        call log_param("amplitude of mode", amp)
+        
+        call log_param("force constant k", f_param%k)
+        call log_param("force constant alpha", f_param%alpha)
+
+        call log_param("full cycle threshold", cycle_threshold)
+    end subroutine
+
+    subroutine log()
+        call write_sep()
+        call log_dp_array(string%y)
+        call log_dp_array(decompose(string, NUM_MODES))
+    end subroutine
+
+    subroutine evolve()
+        string = symp_integrator(string, nonlinear_f, f_param, step_size, order)     
+    end subroutine
 
 end program
---
+++ src/io.f90
@@ -0,0 +1,56 @@
+module io
+    use grids
+    use kind_parameter, only: i4, dp
+
+    implicit none
+
+    interface log_param
+        module procedure log_param_dp, log_param_int
+    end interface
+
+contains
+
+    subroutine log_param_dp(name, param)
+        character(*), intent(in) :: name
+        real(dp), intent(in) :: param
+
+        write(*, '(A2, A20, F8.3)') '% ', name, param
+    end subroutine
+
+    subroutine log_param_int(name, param)
+        character(*), intent(in) :: name
+        integer(i4), intent(in) :: param
+
+        write(*, '(A2, A20, I8)') '% ', name, param
+    end subroutine
+
+    subroutine log_dp_array(array)
+        real(dp), dimension(:), intent(in) :: array
+        write(*, '(*(F9.6,2X))') array
+    end subroutine
+
+    subroutine write_sep(sep, num)
+        character, intent(in), optional :: sep
+        integer(dp), intent(in), optional :: num
+
+        character :: sep_used
+        integer(dp) :: num_used
+
+        if (.not. present(sep)) then
+            sep_used = '-'
+        else
+            sep_used = sep
+        end if
+
+        if (.not. present(num)) then
+            num_used = 5
+        else
+            num_used = num
+        end if
+
+        write(*, *) repeat(sep_used, num_used)
+
+    end subroutine
+
+end module
+
